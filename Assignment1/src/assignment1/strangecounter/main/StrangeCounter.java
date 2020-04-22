cea619db7dac803472591e81ebf9a25d9433aed7
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-04-22 09:16:29 +0200

    Umbenennen von Package Counter
    Das package counter zu main umbenannt.

--- Assignment1/src/assignment1/strangecounter/main/StrangeCounter.java
+++ Assignment1/src/assignment1/strangecounter/main/StrangeCounter.java
@@ -1,4 +1,4 @@
-package assignment1.strangecounter.counter;
+package assignment1.strangecounter.main;
 import java.util.concurrent.CountDownLatch;
 
 import assignment1.strangecounter.mylong.MyLong;
