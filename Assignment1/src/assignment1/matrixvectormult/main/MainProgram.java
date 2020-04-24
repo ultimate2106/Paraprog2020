677e70ba363b7661b15b7377df3a2de93c369bb1
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-04-24 11:57:47 +0200

    Methoden und Variablen verschoben
    Aus den Variablen habe ich jetzt Konstanten gemacht und einige Methoden verschoben und etwas umgeschrieben.

--- Assignment1/src/assignment1/matrixvectormult/main/MainProgram.java
+++ Assignment1/src/assignment1/matrixvectormult/main/MainProgram.java
@@ -2,33 +2,61 @@ package assignment1.matrixvectormult.main;
 
 import java.util.concurrent.ForkJoinPool;
 
+import assignment1.matrixvectormult.utils.MatrixVectorMultiplication;
 import assignment1.matrixvectormult.utils.MatrixVectorUtils;
 
 public class MainProgram {
+	private static final int length = 7;
+	private static double[][] matrix;
+	private static double[] vector;
 	
-	
-	private static void printResult(double[][] matrix,double[] vector,double[] result,int length) 
-	{
-		System.out.println("-----Matrix-----");
-		for(int i=0;i<length;++i) 
-		{
-			for(int j=0;j<length;++j) 
-			{
-				System.out.print(matrix[i][j]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println("-----Vector-----");
-		for(int i=0;i<length;++i) {
-			System.out.println(vector[i]);
-		}
-		System.out.println("-----Result-----");
-		for(int i=0;i<length;++i) {
-			System.out.println(result[i]);
-		}
+	public static void main(String[] args) {
+		matrix = MatrixVectorUtils.getTestMatrix(length);
+		vector = MatrixVectorUtils.getTestVector(length);
+		
+		ForkJoinPool pool1 = new ForkJoinPool(1);
+		ForkJoinPool pool2 = new ForkJoinPool(3);
+		ForkJoinPool pool3 = new ForkJoinPool(7);
+		
+		calc(pool1, 1);
+		calc(pool2, 2);
+		calc(pool3, 3);
 	}
 	
-	private static boolean checkResult(double[][] matrix, double[] vector,double result[]) 
+	private static void calc(ForkJoinPool pool, int poolNr) {
+		double[] result = new double[length];
+		
+		System.out.println("=============Pool " + poolNr + "=============");
+		doIt(pool, matrix, vector, result);
+		printResult(result);
+	}
+	
+	private static void printResult(double[] result) 
+	{
+		if(checkResult(result)) {
+			System.out.println("-----Matrix-----");
+			for(int i=0;i<length;++i) 
+			{
+				for(int j=0;j<length;++j) 
+				{
+					System.out.print(matrix[i][j]+" ");
+				}
+				System.out.println();
+			}
+			System.out.println("-----Vector-----");
+			for(int i=0;i<length;++i) {
+				System.out.println(vector[i]);
+			}
+			System.out.println("-----Result-----");
+			for(int i=0;i<length;++i) {
+				System.out.println(result[i]);
+			}
+		} else {
+			System.out.println("Result was false");
+		}	
+	}
+	
+	private static boolean checkResult(double[] result) 
 	{
 		boolean isResultCorrect=true; 
 		int length=result.length;
@@ -47,31 +75,17 @@ public class MainProgram {
 		return isResultCorrect;
 	}
 	
-	private static void callDoIt(ForkJoinPool pool,MatrixVectorUtils utils,int length,int poolNumber)
-	{
-		System.out.println("=============Pool "+poolNumber+"=============");
-		double[][] matrix = utils.getTestMatrix(length);
-		double[] vector = utils.getTestVector(length);
-		double[] result = new double[length];
-		utils.doIt(pool, matrix, vector, result);
-		if(checkResult(matrix,vector,result)) 
-		{
-			printResult(matrix,vector,result,length);	
-		}
-		else 
-		{
-			System.out.println("Result was false");
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		ForkJoinPool pool1 = new ForkJoinPool(1);
-		ForkJoinPool pool2 = new ForkJoinPool(3);
-		ForkJoinPool pool3 = new ForkJoinPool(7);
-		MatrixVectorUtils utils = new MatrixVectorUtils();		
-		callDoIt(pool1,utils,20,1);
-		callDoIt(pool2,utils,20,2);
-		callDoIt(pool3,utils,20,3);			
+	/**
+	 * Diese Methode nutzt ForkJoinPool für die Multiplikation
+	 * einer Matrix mit einem Vektor.
+	 * 
+	 * @param pool Der ForkJoinPool zur Berechnung. 
+	 * @param matrix Die Matrix für die Multiplikation.
+	 * @param vector Der Vektor für die Multiplikation.
+	 * @param result Der Vektor in dem das Ergebnis gespeichert wird.
+	 */
+	public static void doIt(ForkJoinPool pool, final double[][] matrix, final double[]vector, final double[] result) {
+		MatrixVectorMultiplication task = new MatrixVectorMultiplication(matrix, vector, result, 0, matrix.length);
+		pool.invoke(task);
 	}
 }
--- Assignment1/src/assignment1/matrixvectormult/utils/MatrixVectorUtils.java
+++ Assignment1/src/assignment1/matrixvectormult/utils/MatrixVectorUtils.java
@@ -1,7 +1,6 @@
 package assignment1.matrixvectormult.utils;
 
 import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
 
 public class MatrixVectorUtils {
 	
@@ -10,7 +9,7 @@ public class MatrixVectorUtils {
 	 * @param dim
 	 * @return Quadratische Matrix der größe dim
 	 */
-	public double[][] getTestMatrix(int dim) {
+	public static double[][] getTestMatrix(int dim) {
 		/*double[][] testMatrix = {
 				{1, 1, 1}, 
 				{2, 2, 2}, 
@@ -33,7 +32,7 @@ public class MatrixVectorUtils {
 	 * @param dim
 	 * @return Vektor der größe dim
 	 */
-	public double[] getTestVector(int dim) {
+	public static double[] getTestVector(int dim) {
 		//double[] testVector = {3, 2, 1};
 		Random rand=new Random();
 		double[] testVector=new double[dim];
@@ -42,19 +41,5 @@ public class MatrixVectorUtils {
 			testVector[i]=Math.round((rand.nextDouble()*10) * 100.0) / 100.0;
 		}
 		return testVector;
-	}
-	
-	/**
-	 * Diese Methode nutzt ForkJoinPool für die Multiplikation
-	 * einer Matrix mit einem Vektor.
-	 * 
-	 * @param pool Der ForkJoinPool zur Berechnung. 
-	 * @param matrix Die Matrix für die Multiplikation.
-	 * @param vector Der Vektor für die Multiplikation.
-	 * @param result Der Vektor in dem das Ergebnis gespeichert wird.
-	 */
-	public void doIt(ForkJoinPool pool, final double[][] matrix, final double[]vector, final double[] result) {
-		MatrixVectorMultiplication task = new MatrixVectorMultiplication(matrix, vector, result, 0, matrix.length);
-		pool.invoke(task);
 	}
 }
