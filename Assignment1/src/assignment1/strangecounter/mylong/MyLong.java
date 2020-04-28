831731c8897ab81a6f35e743a6848f677e88549e
Benjamin Scheer <bscheer@studenten.hs-bremerhaven.de>
2020-04-28 10:03:33 +0200

    change comments in MyLong

--- Assignment1/src/assignment1/strangecounter/mylong/MyLong.java
+++ Assignment1/src/assignment1/strangecounter/mylong/MyLong.java
@@ -7,21 +7,14 @@ import assignment1.strangecounter.interfaces.CounterInterface;
  *
  */
 public class MyLong implements CounterInterface {
-	/**
-	 * Zähler des Typs long
-	 */
+
 	private long counter = 0;
-	/**
-	 * Siehe CounterInterface
-	 */
+
 	@Override
 	public long get() {
 		return counter;
 	}
 
-	/**
-	 * Siehe CounterInterface
-	 */
 	@Override
 	public long incrementAndGet() {
 		return ++counter;
--- Assignment1/src/assignment1/strangecounter/mylong/MyLongAtomic.java
+++ Assignment1/src/assignment1/strangecounter/mylong/MyLongAtomic.java
@@ -10,23 +10,14 @@ import assignment1.strangecounter.interfaces.CounterInterface;
  *
  */
 public class MyLongAtomic implements CounterInterface {
-	/**
-	 * Zähler des Typs AtomicLong
-	 */
+
 	protected AtomicLong counter = new AtomicLong();
 	
-	/**
-	 * Gibt den Zählerstand mittels der get Methode von AtomicLong zurück
-	 */
 	@Override
 	public long get() {
 		return counter.get();
 	}
 
-	/**
-	 * Erhöht den aktuellen Zählerstand mittels der incrementAndGet() Methode von AtomicLong
-	 * und gibt den erhöhten Zählerstand zurück.
-	 */
 	@Override
 	public long incrementAndGet() {
 		return counter.incrementAndGet();
