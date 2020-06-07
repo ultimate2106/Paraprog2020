75b8142f9653d2f7ee385b52b2a8de8fa3be1110
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-06-07 09:33:28 +0200

    Neuer Versuch zu Steuerung2
    Habe eig. nicht soviel geändert aber diese "geht" nun.

--- Assignment2/src/main/MainProgram.java
+++ Assignment2/src/main/MainProgram.java
@@ -1,8 +1,7 @@
 package main;
 
 import achterbahn.interfaces.Steuerung;
-import achterbahn.monitor.Steuerung1;
-import achterbahn.monitor.Steuerung2;
+import achterbahn.monitor.*;
 import achterbahn.thread.Drehkreuz;
 import achterbahn.thread.Wagen;
 
@@ -10,17 +9,22 @@ public class MainProgram {
 
 	public static void main(String[] args) {
 		//Steuerung steuerung = new Steuerung1();
-		Steuerung steuerung = new Steuerung2();
+		//Steuerung steuerung = new Steuerung2();
+		Steuerung steuerung = new Steuerung2_NEW();
+		
 		Drehkreuz drehkreuz = new Drehkreuz(steuerung);
+		
 		Wagen wagen = new Wagen(steuerung, true);
 		Wagen wagen2 = new Wagen(steuerung, false);
 
 		Thread wagenThread = new Thread(wagen);
 		Thread wagen2Thread = new Thread(wagen2);
+		
 		Thread drehkreuzThread = new Thread(drehkreuz);
 		
 		wagenThread.start();
 		wagen2Thread.start();
+		
 		drehkreuzThread.start();
 	}
 
