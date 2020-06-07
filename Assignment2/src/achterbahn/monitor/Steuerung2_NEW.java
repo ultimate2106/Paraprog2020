a8478b2bb0701038491a5d73e3c640a38d83f792
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-06-07 15:27:06 +0200

    Update falscher Wächterbedingung
    Die Negierung der Wächterbedingung der passagier-Methode war falsch und wurde nun korrigiert.

--- Assignment2/src/achterbahn/monitor/Steuerung2_NEW.java
+++ Assignment2/src/achterbahn/monitor/Steuerung2_NEW.java
@@ -4,7 +4,7 @@ import achterbahn.interfaces.Steuerung;
 
 public class Steuerung2_NEW implements Steuerung {
 	
-	private final int MaxPassagiere = 25;
+	private final int MaxPassagiere = 5;
 	private int w1Passagiere = 0;
 	private int w2Passagiere = 0;
 	private boolean w1Vorne = false;
@@ -24,24 +24,20 @@ public class Steuerung2_NEW implements Steuerung {
 	@Override
 	public synchronized void passagier() {
 		try {
-			while(!(w1Passagiere < MaxPassagiere) && !(w2Passagiere < MaxPassagiere)) {
-				wait();
-			}
-			
 			double random = Math.random();
 			
-			if(random < 0.5) {
-				if(w1Passagiere < MaxPassagiere) {
-					incrementPassagiereW1();
-				} else {					
-					incrementPassagiereW2();
+			if(random < 0.5 && w1Passagiere < MaxPassagiere) {
+				while(!((w1Passagiere < MaxPassagiere && w2Passagiere < MaxPassagiere) || (w1Passagiere < MaxPassagiere && w1Vorne))) {
+					wait();
 				}
-			} else {
-				if(w2Passagiere < MaxPassagiere) {
-					incrementPassagiereW2();
-				} else {
-					incrementPassagiereW1();
+				
+				incrementPassagiereW1();
+			} else if(w2Passagiere < MaxPassagiere){
+				while(!((w1Passagiere < MaxPassagiere && w2Passagiere < MaxPassagiere) || (w2Passagiere < MaxPassagiere && w2Vorne))) {
+					wait();
 				}
+				
+				incrementPassagiereW2();
 			}
 			
 			notifyAll();
@@ -55,7 +51,7 @@ public class Steuerung2_NEW implements Steuerung {
 		if(w1Passagiere < MaxPassagiere) {
 			System.out.println("Ein Passagier ist in Wagen1 eingestiegen. Aktuelle Anzahl " + w1Passagiere);
 		} else {
-			System.out.println("Wagen1 ist voll!");
+			System.out.println("Wagen1 hat " + MaxPassagiere + " und ist voll!");
 		}
 		
 		try {
@@ -71,7 +67,7 @@ public class Steuerung2_NEW implements Steuerung {
 		if(w2Passagiere < MaxPassagiere) {
 			System.out.println("Ein Passagier ist in Wagen2 eingestiegen. Aktuelle Anzahl " + w2Passagiere);
 		} else {
-			System.out.println("Wagen2 ist voll!");
+			System.out.println("Wagen2 hat " + MaxPassagiere + " und ist voll!");
 		}
 		
 		try {
