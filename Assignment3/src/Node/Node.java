8f561f8cc4b56b59291f64389361357dcce73dda
Benjamin Scheer <bscheer@studenten.hs-bremerhaven.de>
2020-07-14 09:24:17 +0200

    lege Package Node an

---
+++ Assignment3/src/Node/Node.java
@@ -0,0 +1,41 @@
+package Node;
+
+/**
+ * This interface defines the methods of each node participating in the echo
+ * algorithm. These methods can be called by the neighbours of a node.
+ */
+public interface Node {
+
+	/**
+	 * Greetings from a neighbour. Before starting the echo algorithm this message
+	 * is called within <code>setupNeighbours</code> once by every node for all its
+	 * initially known neighbours.
+	 * 
+	 * The main purpose is that <code>this</code> node gets the chance to add the
+	 * calling <code>neighbour</code> to its own list of neighbours (just in case
+	 * the neighbour was previously unknown to this node although the neighbour
+	 * itself knows this node).
+	 * 
+	 * @param neighbour
+	 */
+	public void hello(Node neighbour);
+
+	/**
+	 * Incoming "wakeup" message from a neighbour.
+	 * 
+	 * @param neighbour
+	 */
+	public void wakeup(Node neighbour);
+
+	/**
+	 * Incoming "echo" message from a neighbour. The neighbour can also send some
+	 * data with this echo message. The data object might e.g. contain information
+	 * about edges of the spanning tree known so far; if not needed, set data to
+	 * null.
+	 * 
+	 * @param neighbour
+	 * @param data
+	 */
+	public void echo(Node neighbour, Object data);
+
+}
---
+++ Assignment3/src/Node/NodeAbstract.java
@@ -0,0 +1,46 @@
+package Node;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Abstract implementation of the Node interface.
+ */
+public abstract class NodeAbstract extends Thread implements Node {
+
+	/** Name of this node */
+	protected final String name;
+
+	/** Is this node the initiator of the echo algorithm? */
+	protected final boolean initiator;
+
+	/**
+	 * Collection of known neighbours of this node; only the methods of the
+	 * neighbours in this collection can be called.
+	 */
+	protected final Set<Node> neighbours = new HashSet<Node>();
+
+	/** Abstract constructor of a node */
+	public NodeAbstract(String name, boolean initiator) {
+		super(name);
+		this.name = name;
+		this.initiator = initiator;
+	}
+
+	/**
+	 * Method to setup the list of initially known neighbours; the setup must be
+	 * complete in all nodes before any echo thread is started!
+	 * 
+	 * Be aware that the neighbour relationship is symmetric (if node "a" has node
+	 * "b" as its neighbour, also node "b" must have node "a" as its neighbour)!
+	 * Therefore call method <code>hello</code> of each neighbour here.
+	 */
+	public abstract void setupNeighbours(Node... neighbours);
+
+	/** Utility method to print this node in a readable way */
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
